98a25eeb8e50cfa0e1f00174f6bfe144088f7fb7
JeongkeonLee <vonjobi2@gmail.com>
2020-09-24 10:53:30 +0900

    build(projectSettings): Add Spring boot Project
    Spring boot 프로젝트를 생성하였습니다.

---
+++ ssjd/src/main/java/com/daou/ssjd/SsjdApplication.java
@@ -0,0 +1,13 @@
+package com.daou.ssjd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsjdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsjdApplication.class, args);
+	}
+
+}
---
+++ ssjd/src/test/java/com/daou/ssjd/SsjdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daou.ssjd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsjdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
